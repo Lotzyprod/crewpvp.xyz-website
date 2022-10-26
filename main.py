ba9b95a479946d71e819a93931da8a072ae2d6bd
LotzyProd <trek.ru@yandex.ru>
2022-10-26 13:27:33 +0300

    refactor js pages code

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ app = Flask(__name__)
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-try:
-	connection = mariadb.connect(host=config['database']['host'],user=config['database']['user'], password=config['database']['password'],database=config['database']['database'],port=config['database']['port'], autocommit=True)
-	now = datetime.now().strftime("[%d/%m/%Y %H:%M:%S]")
-	print(f'{now} Connected to the database')
-except mariadb.Error as e:
-	now = datetime.now().strftime("[%d/%m/%Y %H:%M:%S]")
-	print(f'{now} Cant connect to the database with reason: {e}')
-	sys.exit(1)
+#try:
+#	connection = mariadb.connect(host=config['database']['host'],user=config['database']['user'], password=config['database']['password'],database=config['database']['database'],port=int(config['database']['port']), autocommit=True)
+#	now = datetime.now().strftime("[%d/%m/%Y %H:%M:%S]")
+#	print(f'{now} Connected to the database')
+#except mariadb.Error as e:
+#	now = datetime.now().strftime("[%d/%m/%Y %H:%M:%S]")
+#	print(f'{now} Cant connect to the database with reason: {e}')
+#	sys.exit(1)
 
 
 virtualPages = {}
--- static/js/crew.js
+++ static/js/crew.js
@@ -39,174 +39,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
   });
 });
 
-(function(window) {
 
-  var support = { transitions: Modernizr.csstransitions },
-    transEndEventNames = { 'WebkitTransition': 'webkitTransitionEnd', 'MozTransition': 'transitionend', 'OTransition': 'oTransitionEnd', 'msTransition': 'MSTransitionEnd', 'transition': 'transitionend' },
-    transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-    onEndTransition = function( el, callback ) {
-      var onEndCallbackFn = function( ev ) {
-        if( support.transitions ) {
-          if( ev.target != this ) return;
-          this.removeEventListener( transEndEventName, onEndCallbackFn );
-        }
-        if( callback && typeof callback === 'function' ) { callback.call(this); }
-      };
-      if( support.transitions ) {
-        el.addEventListener( transEndEventName, onEndCallbackFn );
-      }
-      else {
-        onEndCallbackFn();
-      }
-    },
-    stack = document.querySelector('.pages-stack'),
-    pages = [].slice.call(stack.children),
-    pagesTotal = pages.length,
-    current = 0,
-    menuCtrl = document.querySelector('button.menu-button'),
-    nav = document.querySelector('.pages-nav'),
-    navItems = [].slice.call(nav.querySelectorAll('.link--page')),
-    isMenuOpen = false;
-
-  function init() {
-    buildStack();
-    initEvents();
-  }
-
-  function buildStack() {
-    var stackPagesIdxs = getStackPagesIdxs();
-
-    for(var i = 0; i < pagesTotal; ++i) {
-      var page = pages[i],
-        posIdx = stackPagesIdxs.indexOf(i);
-
-      if( current !== i ) {
-        page.classList.add('page--inactive');
-
-        if( posIdx !== -1 ) {
-          page.style.WebkitTransform = 'translate3d(0,100%,0)';
-          page.style.transform = 'translate3d(0,100%,0)';
-        }
-        else {
-          page.style.WebkitTransform = 'translate3d(0,75%,-300px)';
-          page.style.transform = 'translate3d(0,75%,-300px)';   
-        }
-      }
-      else {
-        page.classList.remove('page--inactive');
-      }
-
-      page.style.zIndex = i < current ? parseInt(current - i) : parseInt(pagesTotal + current - i);
-      
-      if( posIdx !== -1 ) {
-        page.style.opacity = parseFloat(1 - 0.05 * posIdx);
-      }
-      else {
-        page.style.opacity = 0;
-      }
-    }
-  }
-
-  function initEvents() {
-    menuCtrl.addEventListener('click', toggleMenu);
-
-    navItems.forEach(function(item) {
-      var pageid = item.getAttribute('href').slice(1);
-      item.addEventListener('click', function(ev) {
-        ev.preventDefault();
-        openPage(pageid);
-      });
-    });
-
-    pages.forEach(function(page) {
-      var pageid = page.getAttribute('id');
-      page.addEventListener('click', function(ev) {
-        if( isMenuOpen ) {
-          ev.preventDefault();
-          openPage(pageid);
-        }
-      });
-    });
-  }
-
-  function toggleMenu() {
-    if( isMenuOpen ) {
-      closeMenu();
-    }
-    else {
-      openMenu();
-      isMenuOpen = true;
-    }
-  }
-
-  function openMenu() {
-    menuCtrl.classList.add('menu-button--open');
-    stack.classList.add('pages-stack--open');
-    nav.classList.add('pages-nav--open');
-
-    var stackPagesIdxs = getStackPagesIdxs();
-    for(var i = 0, len = stackPagesIdxs.length; i < len; ++i) {
-      var page = pages[stackPagesIdxs[i]];
-      page.style.WebkitTransform = 'translate3d(0, 75%, ' + parseInt(-1 * 200 - 50*i) + 'px)'; 
-      page.style.transform = 'translate3d(0, 75%, ' + parseInt(-1 * 200 - 50*i) + 'px)';
-    }
-  }
-
-  function closeMenu() {
-    openPage();
-  }
-
-  function openPage(id) {
-    var futurePage = id ? document.getElementById(id) : pages[current],
-      futureCurrent = pages.indexOf(futurePage),
-      stackPagesIdxs = getStackPagesIdxs(futureCurrent);
-
-    futurePage.style.WebkitTransform = 'translate3d(0, 0, 0)';
-    futurePage.style.transform = 'translate3d(0, 0, 0)';
-    futurePage.style.opacity = 1;
-
-    for(var i = 0, len = stackPagesIdxs.length; i < len; ++i) {
-      var page = pages[stackPagesIdxs[i]];
-      page.style.WebkitTransform = 'translate3d(0,100%,0)';
-      page.style.transform = 'translate3d(0,100%,0)';
-    }
-
-    if( id ) {
-      current = futureCurrent;
-    }
-    
-    menuCtrl.classList.remove('menu-button--open');
-    nav.classList.remove('pages-nav--open');
-    onEndTransition(futurePage, function() {
-      stack.classList.remove('pages-stack--open');
-      buildStack();
-      isMenuOpen = false;
-    });
-  }
-
-  function getStackPagesIdxs(excludePageIdx) {
-    var nextStackPageIdx = current + 1 < pagesTotal ? current + 1 : 0,
-      nextStackPageIdx_2 = current + 2 < pagesTotal ? current + 2 : 1,
-      idxs = [],
-
-      excludeIdx = excludePageIdx || -1;
-
-    if( excludePageIdx != current ) {
-      idxs.push(current);
-    }
-    if( excludePageIdx != nextStackPageIdx ) {
-      idxs.push(nextStackPageIdx);
-    }
-    if( excludePageIdx != nextStackPageIdx_2 ) {
-      idxs.push(nextStackPageIdx_2);
-    }
-
-    return idxs;
-  }
-
-  init();
-
-})(window)
 
 class TextScramble {
   constructor(el) {
@@ -276,7 +109,182 @@ function fadeOut(e,t) {
     }, t);
 }
 
-window.addEventListener('load', function (e) {
-    var loader = document.querySelector('#page-preloader');
-    setTimeout(fadeOut(loader,50), 350);
-})
+window.addEventListener('load', function (e) { setTimeout(fadeOut(document.querySelector('#page-preloader'),50), 350); })
+
+class PagesStack {
+  constructor() {
+    this.support = { transitions: Modernizr.csstransitions };
+    this.transEndEventNames = { 'WebkitTransition': 'webkitTransitionEnd', 'MozTransition': 'transitionend', 'OTransition': 'oTransitionEnd', 'msTransition': 'MSTransitionEnd', 'transition': 'transitionend' };
+    this.transEndEventName = this.transEndEventNames[ Modernizr.prefixed( 'transition' ) ];
+    this.onEndTransition = ( el, callback ) => {
+      var onEndCallbackFn = ( ev ) => {
+        if( this.support.transitions ) {
+          if( ev.target != el ) return;
+          el.removeEventListener( this.transEndEventName, onEndCallbackFn );
+        }
+        if( callback && typeof callback === 'function' ) { callback.call(el); }
+      };
+      if( this.support.transitions ) {
+        el.addEventListener( this.transEndEventName, onEndCallbackFn );
+      }
+      else {
+        onEndCallbackFn();
+      }
+    };
+
+    this.stack = document.querySelector('.pages-stack');
+    this.pages = [].slice.call(this.stack.children);
+    this.pagesTotal = this.pages.length;
+    this.current = 0;
+    this.menuCtrl = document.querySelector('button.menu-button');
+    this.nav = document.querySelector('.pages-nav');
+    this.navItems = [].slice.call(this.nav.querySelectorAll('.link--page'));
+    this.isMenuOpen = false;
+    this.buildStack();
+    this.initEvents();
+  }
+
+  addPage(page,nav) {
+    this.pagesTotal++;
+    document.querySelector('.pages-stack').innerHTML+=page;
+    document.querySelector('.pages-nav').innerHTML+=nav;
+    this.stack = document.querySelector('.pages-stack');
+    this.pages = [].slice.call(this.stack.children);
+    this.nav = document.querySelector('.pages-nav');
+    this.navItems = [].slice.call(nav.querySelectorAll('.link--page'));
+    if (this.isMenuOpen) {
+      page.style.WebkitTransform = 'translate3d(0, 75%, ' + parseInt(-1 * 200 - 50*i) + 'px)'; 
+      page.style.transform = 'translate3d(0, 75%, ' + parseInt(-1 * 200 - 50*i) + 'px)';
+    }
+  }
+
+  getStackPagesIdxs(excludePageIdx) {
+    var nextStackPageIdx = this.current + 1 < this.pagesTotal ? this.current + 1 : 0,
+      nextStackPageIdx_2 = this.current + 2 < this.pagesTotal ? this.current + 2 : 1,
+      idxs = [], excludeIdx = excludePageIdx || -1;
+
+    if( excludePageIdx != this.current ) {
+      idxs.push(this.current);
+    }
+    if( excludePageIdx != nextStackPageIdx ) {
+      idxs.push(nextStackPageIdx);
+    }
+    if( excludePageIdx != nextStackPageIdx_2 ) {
+      idxs.push(nextStackPageIdx_2);
+    }
+
+    return idxs;
+  }
+
+  buildStack() {
+    var stackPagesIdxs = this.getStackPagesIdxs();
+
+    for(var i = 0; i < this.pagesTotal; ++i) {
+      var page = this.pages[i],
+        posIdx = stackPagesIdxs.indexOf(i);
+
+      if( this.current !== i ) {
+        page.classList.add('page--inactive');
+
+        if( posIdx !== -1 ) {
+          page.style.WebkitTransform = 'translate3d(0,100%,0)';
+          page.style.transform = 'translate3d(0,100%,0)';
+        }
+        else {
+          page.style.WebkitTransform = 'translate3d(0,75%,-300px)';
+          page.style.transform = 'translate3d(0,75%,-300px)';   
+        }
+      }
+      else {
+        page.classList.remove('page--inactive');
+      }
+
+      page.style.zIndex = i < this.current ? parseInt(this.current - i) : parseInt(this.pagesTotal + this.current - i);
+      
+      if( posIdx !== -1 ) {
+        page.style.opacity = parseFloat(1 - 0.05 * posIdx);
+      }
+      else {
+        page.style.opacity = 0;
+      }
+    }
+  }
+
+  openPage(id) {
+    var futurePage = id ? document.getElementById(id) : this.pages[this.current],
+      futureCurrent = this.pages.indexOf(futurePage),
+      stackPagesIdxs = this.getStackPagesIdxs(futureCurrent);
+
+    futurePage.style.WebkitTransform = 'translate3d(0, 0, 0)';
+    futurePage.style.transform = 'translate3d(0, 0, 0)';
+    futurePage.style.opacity = 1;
+
+    for(var i = 0, len = stackPagesIdxs.length; i < len; ++i) {
+      var page = this.pages[stackPagesIdxs[i]];
+      page.style.WebkitTransform = 'translate3d(0,100%,0)';
+      page.style.transform = 'translate3d(0,100%,0)';
+    }
+
+    if( id ) {
+      this.current = futureCurrent;
+    }
+    
+    this.menuCtrl.classList.remove('menu-button--open');
+    this.nav.classList.remove('pages-nav--open');
+    this.onEndTransition(futurePage, ev => {
+      this.stack.classList.remove('pages-stack--open');
+      this.buildStack();
+      this.isMenuOpen = false;
+    });
+  }
+
+  openMenu() {
+    this.menuCtrl.classList.add('menu-button--open');
+    this.stack.classList.add('pages-stack--open');
+    this.nav.classList.add('pages-nav--open');
+
+    var stackPagesIdxs = this.getStackPagesIdxs();
+    for(var i = 0, len = stackPagesIdxs.length; i < len; ++i) {
+      var page = this.pages[stackPagesIdxs[i]];
+      page.style.WebkitTransform = 'translate3d(0, 75%, ' + parseInt(-1 * 200 - 50*i) + 'px)'; 
+      page.style.transform = 'translate3d(0, 75%, ' + parseInt(-1 * 200 - 50*i) + 'px)';
+    }
+  }
+
+  closeMenu() {
+    this.openPage();
+  }
+
+  toggleMenu() {
+    if( this.isMenuOpen ) {
+      this.closeMenu();
+    } else {
+      this.openMenu();
+      this.isMenuOpen = true;
+    }
+  }
+
+  initEvents() {
+    this.menuCtrl.addEventListener('click', this.toggleMenu.bind(this));
+
+    this.navItems.forEach(item => {
+      var pageid = item.getAttribute('href').slice(1);
+      item.addEventListener('click', ev => {
+        ev.preventDefault();
+        this.openPage(pageid);
+      });
+    });
+
+    this.pages.forEach(page => {
+      var pageid = page.getAttribute('id');
+      page.addEventListener('click', ev => {
+        if( this.isMenuOpen ) {
+          ev.preventDefault();
+          this.openPage(pageid);
+        }
+      });
+    });
+  }
+}
+
+var ps = new PagesStack(); 
